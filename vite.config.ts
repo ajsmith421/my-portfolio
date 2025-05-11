e32a88030c6471ca597049fe9d01e11c0a92e5cf
andrew smith <andrewsmith@andrews-mbp-2.mynetworksettings.com>
2025-05-11 03:42:37 -0400

    Allow Gitpod host

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    strictPort: true,
+    host: '0.0.0.0',
     port: 5173,
-    allowedHosts: ['5173-ajsmith421-myportfolio-3v6etbxk6kb.ws-us118.gitpod.io'],
+    strictPort: true,
   },
 })
+
+
+
