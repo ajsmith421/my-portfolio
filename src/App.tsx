2f050daf5298d554b310a693a663172403b348c9
andrew smith <andrewsmith@andrews-MacBook-Pro-2.local>
2025-05-12 19:31:20 -0400

    fixed website title

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 export default function App() {
   useEffect(() => {
-    document.title = "Your Name | Software Developer";
+    document.title = "Andrew Smith | Software Developer";
   }, []);
 
   return (
